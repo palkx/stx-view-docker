abefefeed25dbfe82e3630fc0647e664101697ac
Mikhail K <m.kravtsov@os-system.com>
2020-10-01 09:21:47 +0300

    stxinsite/stx-view#81: Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 set -xe;
 IMAGES_DIR=.;
 source params.sh;
-env;
 docker login --username="${DOCKER_USER}" --password="${DOCKER_PASSWORD}";
 
 if [ "$(git diff --name-only HEAD..HEAD^1 | grep -E "^(core\.Dockerfile|build\.sh|params\.sh)$")" != "" ]; then
@@ -20,7 +19,7 @@ if [ "$(git diff --name-only HEAD..HEAD^1 | grep -E "^(core\.Dockerfile|build\.s
   docker push "$BASE"/oss-stx-rdkit-core:latest;
 fi
 
-docker pull "$BASE"/oss-stx-rdkit-core:latest;
+docker pull "$BASE"/oss-stx-rdkit-core:$DOCKER_TAG;
 rm -rf artifacts/debian/"$DOCKER_TAG";
 mkdir -p artifacts/debian/"$DOCKER_TAG";
 mkdir -p artifacts/debian/"$DOCKER_TAG"/debs;
@@ -30,9 +29,12 @@ docker run -it --rm -u "$(id -u)"\
   -v "$PWD"/artifacts/debian/"$DOCKER_TAG":/tohere:Z\
   "$BASE"/oss-stx-rdkit-core:"$DOCKER_TAG" bash -c 'cp build/*.deb /tohere/debs && cp tmalign /tohere/tmalign/ && mkdir -p /tohere/mmseqs2/sse && mkdir /tohere/mmseqs2/avx && cp /mmseqs2/build_sse/bin/mmseqs /tohere/mmseqs2/sse/mmseqs && cp /mmseqs2/build_avx/bin/mmseqs /tohere/mmseqs2/avx/mmseqs';
 
+# List existing artifacts
+tree -d artifacts || ls -la artifacts/debian
+
 if [ "$(git diff --name-only HEAD..HEAD^1 | grep -E "^(python3\.Dockerfile|build\.sh|params\.sh)$")" != "" ]; then
   # build image for python3 on debian
-  docker pull "$BASE"/oss-stx-rdkit-python3:latest;
+  docker pull "$BASE"/oss-stx-rdkit-python3:latest || true;
   docker build -f $IMAGES_DIR/python3.Dockerfile\
     -t "$BASE"/oss-stx-rdkit-python3:"$DOCKER_TAG"\
     -t "$BASE"/oss-stx-rdkit-python3:latest\
@@ -44,7 +46,7 @@ fi
 
 if [ "$(git diff --name-only HEAD..HEAD^1 | grep -E "^(python3-with-torch\.Dockerfile|build\.sh|params\.sh)$")" != "" ]; then
   # build image for python3 with torch on debian
-  docker pull "$BASE"/oss-stx-rdkit-python3-torch:latest;
+  docker pull "$BASE"/oss-stx-rdkit-python3-torch:latest || true;
   docker build -f $IMAGES_DIR/python3-with-torch.Dockerfile\
     -t "$BASE"/oss-stx-rdkit-python3-torch:"$DOCKER_TAG"\
     -t "$BASE"/oss-stx-rdkit-python3-torch:latest\
@@ -56,7 +58,7 @@ fi
 
 if [ "$(git diff --name-only HEAD..HEAD^1 | grep -E "^(cartridge\.Dockerfile|build\.sh|params\.sh)$")" != "" ]; then
   # build image for postgresql cartridge on debian
-  docker pull "$BASE"/oss-stx-rdkit-cartridge:latest;
+  docker pull "$BASE"/oss-stx-rdkit-cartridge:latest || true;
   docker build -f $IMAGES_DIR/cartridge.Dockerfile\
     -t "$BASE"/oss-stx-rdkit-cartridge:"$DOCKER_TAG"\
     -t "$BASE"/oss-stx-rdkit-cartridge:latest\
@@ -68,7 +70,7 @@ fi
 
 if [ "$(git diff --name-only HEAD..HEAD^1 | grep -E "^(tt\.Dockerfile|build\.sh|params\.sh|targettrackparser)$")" != "" ]; then
   # Create storage container with Target Track database
-  docker pull "$BASE"/oss-stx-tt-storage:latest;
+  docker pull "$BASE"/oss-stx-tt-storage:latest || true;
   docker build -f $IMAGES_DIR/tt.Dockerfile\
     -t "$BASE"/oss-stx-tt-storage:"$DOCKER_TAG"\
     -t "$BASE"/oss-stx-tt-storage:latest\
@@ -90,7 +92,8 @@ if [ "$(git diff --name-only HEAD..HEAD^1 | grep -E "^(null\.Dockerfile|build\.s
 fi
 
 if [ "$(git diff --name-only HEAD..HEAD^1 | grep -E "^(python3-gu\.Dockerfile|build\.sh|params\.sh)$")" != "" ]; then
-  # empty image
+  # lightweight backend
+  docker pull "$BASE"/oss-stx-python3-gu:latest || true;
   docker build -t "$BASE"/oss-stx-python3-gu:"$DOCKER_TAG"\
     -t "$BASE"/oss-stx-python3-gu:latest\
     --build-arg DOCKER_TAG="$DOCKER_TAG" - < "$IMAGES_DIR"/python3-gu.Dockerfile;
