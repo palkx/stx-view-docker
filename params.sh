e6251076bc8a70ba89a07e1d2dfb126d6cc93986
Mikhail K <m.kravtsov@os-system.com>
2020-10-26 12:28:25 +0200

    stxinsite/stx-view#97: Bump rdkit to 2020_09_01, release v1.1.2

--- params.sh
+++ params.sh
@@ -1,9 +1,9 @@
-export DOCKER_TAG=v1.1.1
+export DOCKER_TAG=v1.1.2
 export GIT_REPO=https://github.com/rdkit/rdkit.git
-export GIT_BRANCH=Release_2019_09
-#export GIT_TAG=Release_2019_09_3
+export GIT_BRANCH=Release_2020_09
+export GIT_TAG=Release_2020_09_1
 
-unset GIT_TAG
+# unset GIT_TAG
 
 export BASE=mikkra
 echo "DOCKER_TAG=$DOCKER_TAG GIT_BRANCH=$GIT_BRANCH no tag"
