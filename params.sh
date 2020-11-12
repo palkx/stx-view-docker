75743668447a04b600003d4fb0cdb67dff470ec1
Mikhail K <m.kravtsov@os-system.com>
2020-11-12 14:02:02 +0200

    stxinsite/stx-view#97: Rollback rdkit version to 2019_3

--- params.sh
+++ params.sh
@@ -1,8 +1,8 @@
 # hash: e6251076bc8a70ba89a07e1d2dfb126d6cc93986
 export DOCKER_TAG=v1.1.2
 export GIT_REPO=https://github.com/rdkit/rdkit.git
-export GIT_BRANCH=Release_2020_09
-export GIT_TAG=Release_2020_09_1
+export GIT_BRANCH=Release_2019_09
+export GIT_TAG=Release_2019_09_3
 
 # unset GIT_TAG
 
